9dbce2e24c73d3bac05cdbb5dda8d310c52aa939
abhishekg785 <abhishekg785@gmail.com>
2017-06-26 15:19:23 +0530

    readme updated

---
+++ test/indexController_test.js
@@ -0,0 +1,10 @@
+/**
+ * author : abhishek goswami
+ * abhishekg785@gmail.com
+ *
+ * indexController_test.js : unit testing for the indexController file
+ */
+
+var path = require('path');
+var assert = require('chai').assert;
+
