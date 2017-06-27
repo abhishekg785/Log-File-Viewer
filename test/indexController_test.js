4035d6d3ab6e9214ddcd70ff0c01b4f6c46b33ff
abhishekg785 <abhishekg785@gmail.com>
2017-06-27 13:57:36 +0530

    test for index route added

--- test/indexController_test.js
+++ test/indexController_test.js
@@ -5,6 +5,25 @@
  * indexController_test.js : unit testing for the indexController file
  */
 
-var path = require('path');
-var assert = require('chai').assert;
+'use strict'
 
+let path = require('path');
+let assert = require('chai').assert;
+
+let request = require('request');
+
+describe('testing indexController module', function () {
+    var indexGETRoute;
+    beforeEach(function() {
+        indexGETRoute = 'http://localhost:3000/';
+    });
+    describe('checking the get index route', function() {
+        it('GET request on the route, req status code must be 200', function(done) {
+            request(indexGETRoute, function(err, res) {
+                assert.equal(res.statusCode, 200);
+                done();
+            });
+        });
+    });
+
+});
